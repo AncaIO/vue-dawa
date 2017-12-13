4e2fbd6cd891446bf24e8a0b655ec2d3038d71a9
Anca Enache <hello@anca.io>
2017-12-13 13:53:57 +0100

    Add explicity type="text" on input

--- src/VueDawa.vue
+++ src/VueDawa.vue
@@ -2,7 +2,7 @@
     <div class="autocomplete-container" :class="containerClasses" :id="containerId">
       <form @submit.prevent autocomplete="off">
         <slot name="label-top"></slot>
-        <input :class="fieldClasses"
+        <input type="text" :class="fieldClasses"
                 v-focus.lazy="inputFocused"
                 :placeholder="placeholder"
                 :id="fieldId"
