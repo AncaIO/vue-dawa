0582639578b18162524369e89a3d6df0b6d4fe48
Anca A.P. Emcken <hello@anca.io>
2020-03-31 11:09:47 +0200

    Update webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   configureWebpack: {
+    devtool: 'inline-cheap-module-source-map',
+    externals: [nodeExternals()],
     output: {
       libraryExport: 'default'
     }
